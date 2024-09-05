9ff5e6266163db8ad54c5eafdf936d1a95fb57b2
bene1310 <beneytan5@gmail.com>
2024-09-05 20:57:40 +0300

    After section 3

--- bastion_connect.sh
+++ bastion_connect.sh
@@ -1,1 +1,34 @@
 #!/bin/bash
+
+# Check if the KEY_PATH environment variable is set
+if [ -z "$KEY_PATH" ]; then
+    echo "KEY_PATH env var is expected"
+    exit 5
+fi
+
+# Check if at least one argument is provided (public instance IP)
+if [ $# -lt 1 ]; then
+    echo "Please provide bastion IP address"
+    exit 5
+fi
+
+# Assign variables based on provided arguments
+PUBLIC_IP=$1
+PRIVATE_IP=$2
+REMOTE_COMMAND=$3
+
+# Check if private IP is provided
+if [ -z "$PRIVATE_IP" ]; then
+    # Connect to the public instance (bastion host)
+    ssh -i "$KEY_PATH" ubuntu@"$PUBLIC_IP"
+else
+    # Connect to the private instance through the bastion host
+    if [ -z "$REMOTE_COMMAND" ]; then
+        # If no command is provided, establish an interactive SSH session
+        ssh -i "$KEY_PATH" -o ProxyCommand="ssh -i $KEY_PATH -W %h:%p ubuntu@$PUBLIC_IP" ubuntu@$PRIVATE_IP
+    else
+        # If a command is provided, execute it on the private instance
+        ssh -i "$KEY_PATH" -o ProxyCommand="ssh -i $KEY_PATH -W %h:%p ubuntu@$PUBLIC_IP" ubuntu@$PRIVATE_IP "$REMOTE_COMMAND"
+    fi
+fi
+
--- ssh_keys_rotation.sh
+++ ssh_keys_rotation.sh
@@ -1,2 +1,36 @@
+  GNU nano 7.2                                                                                   ssh_keys_rotation.sh
 #!/bin/bash
 
+# Check if private instance IP is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <private-instance-ip>"
+  exit 1
+fi
+
+PRIVATE_INSTANCE_IP="$1"
+
+# Paths to old and new keys
+OLD_KEY_PATH=~/.ssh/id_rsa
+NEW_KEY_PATH=~/.ssh/id_rsa_new
+
+# Generate new SSH key pair
+ssh-keygen -t rsa -b 4096 -f $NEW_KEY_PATH -N ""
+
+# Copy new public key to private instance's authorized_keys
+scp -i $OLD_KEY_PATH $NEW_KEY_PATH.pub ubuntu@$PRIVATE_INSTANCE_IP:/home/ubuntu/.ssh
+ssh -i $OLD_KEY_PATH ubuntu@$PRIVATE_INSTANCE_IP "mkdir -p ~/.ssh && echo $(cat $NEW_KEY_PATH.pub) >> ~/.ssh/authorized_keys"
+
+# Remove old key from authorized_keys on private instance
+OLD_PUBLIC_KEY=$(cat $OLD_KEY_PATH.pub)
+CLEAR_OLD_KEY=$(echo "$OLD_PUBLIC_KEY" | sed 's/[\/&]/\\&/g')
+ssh -i $NEW_KEY_PATH ubuntu@$PRIVATE_INSTANCE_IP "sed -i '/$CLEAR_OLD_KEY/d' ~/.ssh/authorized_keys"
+
+# Clean up old key if necessary (optional, usually done after verifying access with the new key)
+rm -f $OLD_KEY_PATH $OLD_KEY_PATH.pub
+
+# Rename the new key without *_new
+mv $NEW_KEY_PATH $HOME/.ssh/id_rsa
+mv $NEW_KEY_PATH.pub $HOME/.ssh/id_rsa.pub
+echo "A key has been successfully replaced on the local instance."
+
+echo "SSH key rotation ended successfully."
