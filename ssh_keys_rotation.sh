6da71d49597ac147b5cfb4172ac7ba72bbe50fac
bene1310 <beneytan5@gmail.com>
2024-09-07 01:28:49 +0300

    After section 4 take 23

--- ssh_keys_rotation.sh
+++ ssh_keys_rotation.sh
@@ -27,11 +27,12 @@ ssh -i $OLD_KEY_PATH ubuntu@$PRIVATE_INSTANCE_IP "mkdir -p ~/.ssh && echo $(cat 
 # Remove old key from authorized_keys on private instance
 OLD_PUBLIC_KEY=$(cat $OLD_KEY_PATH.pub)
 CLEAR_OLD_KEY=$(echo "$OLD_PUBLIC_KEY" | sed 's/[\/&]/\\&/g')
-ssh -i $NEW_KEY_PATH ubuntu@$PRIVATE_INSTANCE_IP "sed -i '/$CLEAR_OLD_KEY/d' ~/.ssh/authorized_keys"
-
 OLD_PUBLIC_GIT_KEY=$(cat $GIT_KEY_PATH.pub)
 CLEAR_OLD_GIT_KEY=$(echo "$OLD_PUBLIC_GIT_KEY" | sed 's/[\/&]/\\&/g')
-ssh -i $NEW_KEY_PATH ubuntu@$PRIVATE_INSTANCE_IP "sed -i '/$CLEAR_OLD_GIT_KEY/d' ~/.ssh/authorized_keys"
+ssh -i $NEW_KEY_PATH ubuntu@$PRIVATE_INSTANCE_IP "sed -i '/$CLEAR_OLD_KEY/d' ~/.ssh/authorized_keys && sed -i '/$CLEAR_OLD_GIT_KEY/d' ~/.ssh/authorized_keys"
+
+
+
 
 # Clean up old key if necessary (optional, usually done after verifying access with the new key)
 rm -f $OLD_KEY_PATH $OLD_KEY_PATH.pub
