f40b1cae7bb90c059b2df4c9acd19ac24321c226
bene1310 <beneytan5@gmail.com>
2024-09-06 15:06:43 +0300

    After section 4 take 10

--- bastion_connect.sh
+++ bastion_connect.sh
@@ -1,30 +1,26 @@
 #!/bin/bash
 
-# Check if KEY_PATH is set
+# Check if the KEY_PATH environment variable is set
 if [ -z "$KEY_PATH" ]; then
-  echo "KEY_PATH env var is expected"
-  exit 5
+    echo "KEY_PATH env var is expected"
+    exit 5
 fi
 
-# Check if at least one argument is provided
+# Check if at least one argument is provided (public instance IP)
 if [ $# -lt 1 ]; then
-  echo "Please provide bastion IP address"
-  exit 5
+    echo "Please provide bastion IP address"
+    exit 5
 fi
 
-# Assign input arguments
-BASTION_IP=$1
+# Assign variables based on provided arguments
+PUBLIC_IP=$1
 PRIVATE_IP=$2
-COMMAND=${@:3}
+REMOTE_COMMAND=$3
 
-# If a private IP is provided, connect to the private instance via the bastion host
-if [ -n "$PRIVATE_IP" ]; then
-  if [ -n "$COMMAND" ]; then
-    ssh -i "$KEY_PATH" -o ProxyCommand="ssh -i $KEY_PATH -W %h:%p ubuntu@$BASTION_IP" ubuntu@$PRIVATE_IP $COMMAND
-  else
-    ssh -i "$KEY_PATH" -o ProxyCommand="ssh -i $KEY_PATH -W %h:%p ubuntu@$BASTION_IP" ubuntu@$PRIVATE_IP
-  fi
+# Check if private IP is provided
+if [ -z "$PRIVATE_IP" ]; then
+    # Connect to the public instance (bastion host)
+    ssh -i "$KEY_PATH" ubuntu@"$PUBLIC_IP"
 else
-  # If no private IP is provided, connect to the bastion host
-  ssh -i "$KEY_PATH" ubuntu@$BASTION_IP
+    ssh -i "$KEY_PATH" -o ProxyCommand="ssh -i $KEY_PATH -W %h:%p ubuntu@$PUBLIC_IP" ubuntu@$PRIVATE_IP "$REMOTE_COMMAND"
 fi
