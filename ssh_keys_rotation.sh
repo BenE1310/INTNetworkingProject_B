94bc5d46b76f2930087ce7c7711e23770cc38214
bene1310 <beneytan5@gmail.com>
2024-09-08 21:45:28 +0300

    After section 4 take 25

--- ssh_keys_rotation.sh
+++ ssh_keys_rotation.sh
@@ -1,4 +1,3 @@
-  GNU nano 7.2                                                                                   ssh_keys_rotation.sh
 #!/bin/bash
 
 # Check if private instance IP is provided
@@ -13,7 +12,7 @@ PRIVATE_INSTANCE_IP="$1"
 # Paths to old and new keys
 OLD_KEY_PATH=~/.ssh/id_rsa
 NEW_KEY_PATH=~/.ssh/id_rsa_new
-GIT_KEY_PATH=/home/ben/DevOps/INTNetworkingProject_B\github_test_ssh_key.pub
+GIT_KEY_PATH=~/.ssh/github_test_ssh_key.pub
 
 
 
@@ -27,8 +26,9 @@ ssh -i $OLD_KEY_PATH ubuntu@$PRIVATE_INSTANCE_IP "mkdir -p ~/.ssh && echo $(cat 
 # Remove old key from authorized_keys on private instance
 OLD_PUBLIC_KEY=$(cat $OLD_KEY_PATH.pub)
 CLEAR_OLD_KEY=$(echo "$OLD_PUBLIC_KEY" | sed 's/[\/&]/\\&/g')
-OLD_PUBLIC_GIT_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDBv1ttal/Vch8LJq+7flHKfvzcDgdiYg7Z1MCjqsZK08CLrAfVWLLER/JpTnX8WBXxzmms3yYon2FP4btsjczuXfes1Xd17UUiu4+kLT7DRl9EcAMuJvGMehDjoVnczHChJBIglM+YbgiG7dsE0Rf8CwvijvZ3CAyvZLTfpc4qiDOt55krFe7GZrtRVAo2Kstaw2ptEWO+BPSh+lWVRg1Sai3/jdiXfMN+W9TdILAwFU9gtRVVyMTioPgp0EPgeW2ttf215sZIDDp1oyFfP0ERT87eOY/H2OvzNDAX8Gxqp2OZtdzmnjenWNXmB5JsKT2lGXv/+2vuq/d08Jf/MJmIhsMuYpAVVzBBBE8a/BKxWpa/caCRAfRHHy4KV4SX8PlP9tMaZh+9Jggio0PPRgG9JflnavfnSX5oS8ypvcAPmCqfRJk1PQeWkuuZxRCCUglTnTv30MiH8WuiE3BtrCKt85G68o0D9SvCiJ721fQ3bg7oUvDHR1NhO9VJA8jlnwPRftpmkk82xAybARr+hJH2Qzbsxd4jQI0SQoyTviiKjAFFvhnhlshkkIC5Ikm08Vhnnrqq8FZSl+eSFUm3pJplnldVtT1nuPW/mejnUXLwiXVA3HddukfF8Zyu5LTMW5y5FCrF7ViX9ltVAFBSGR5SrYsGjb8tYZH+U889DDKJ5w== beneytan@gmail.com"
-ssh -i $NEW_KEY_PATH ubuntu@$PRIVATE_INSTANCE_IP "sed -i '/$CLEAR_OLD_KEY/d' ~/.ssh/authorized_keys && sed -i '/${OLD_PUBLIC_GIT_KEY}/d' ~/.ssh/authorized_keys"
+OLD_PUBLIC_GIT_KEY=$(cat $GIT_KEY_PATH)
+CLEAR_OLD_GIT_KEY=$(echo "$OLD_PUBLIC_GIT_KEY" | sed 's/[\/&]/\\&/g')
+ssh -i $NEW_KEY_PATH ubuntu@$PRIVATE_INSTANCE_IP "sed -i '/$CLEAR_OLD_KEY/d' ~/.ssh/authorized_keys && sed -i '/${CLEAR_OLD_GIT_KEY}/d' ~/.ssh/authorized_keys"
 
 
 
